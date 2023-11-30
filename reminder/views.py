b220ce03ed2b5bbd6403dc5f80cd5b86c68adfa3
Joel Emmanuel <ejoel0035@gmail.com>
2023-11-30 14:56:52 +0100

    build: simulate chatbot

--- reminder/views.py
+++ reminder/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from .models import Medication
+import random
 
 def medication_list(request):
     medications = Medication.objects.all()
     return render(request, 'medication_list.html', {'medications': medications})
 
+def chatbot(request):
+    if request.method == 'POST':
+        user_message = request.POST.get('user_message')
+
+        # Simulate a chatbot response (replace this with your actual chatbot logic)
+        bot_response = simulate_chatbot_response(user_message)
+
+        return JsonResponse({'bot_response': bot_response})
+
+    return render(request, 'chatbot.html')
