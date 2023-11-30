1d3e2c63d455fd781ddc85f88078f561b7f7a299
Joel Emmanuel <ejoel0035@gmail.com>
2023-11-30 15:08:45 +0100

    refactor: Update Project URLs

--- reminder/urls.py
+++ reminder/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import medication_list
+from .views import medication_list, chatbot
 
 urlpatterns = [
     path('list/', medication_list, name='medication_list'),
+    path('chatbot/', chatbot, name='chatbot')
 ]
