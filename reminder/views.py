1f8cb29862c5d71136b77189a85d455e94c23a76
Joel Emmanuel <ejoel0035@gmail.com>
2023-12-02 15:56:46 +0100

    feat: add api key to views

--- reminder/views.py
+++ reminder/views.py
@@ -1,18 +1,11 @@
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
 from django.http import JsonResponse
-from chatterbot import ChatBot
-from chatterbot.trainers import ChatterBotCorpusTrainer
+import openai
 from .models import Medication
 
-# Create a new chat bot
-chatbot_instance = ChatBot('MedicationBot')
-
-# Create a new Trainer for the chat bot
-trainer = ChatterBotCorpusTrainer(chatbot_instance)
-
-# Train the chat bot on English language data
-trainer.train('chatterbot.corpus.english')
+# Set your OpenAI API key
+openai.api_key = 'your_openai_api_key'
 
 def medication_list(request):
     medications = Medication.objects.all()
@@ -23,9 +16,21 @@ def chatbot_view(request):
     if request.method == 'POST':
         user_message = request.POST.get('user_message')
 
-        # Get a response from the chat bot
-        bot_response = chatbot_instance.get_response(user_message).text
+        # Make a request to the ChatGPT API
+        chatgpt_response = generate_chatgpt_response(user_message)
+
+        # Extract the bot response from the ChatGPT API response
+        bot_response = chatgpt_response['choices'][0]['text']
 
         return JsonResponse({'bot_response': bot_response})
 
     return render(request, 'chatbot.html')
+
+def generate_chatgpt_response(user_message):
+    # Make a request to the ChatGPT API
+    response = openai.Completion.create(
+        engine="text-davinci-003",  # Specify the ChatGPT engine
+        prompt=user_message,
+        max_tokens=100,
+    )
+    return response
